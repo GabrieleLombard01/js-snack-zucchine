f72c4d085ecceaf2ff8dc40b22e2beae3af3e187
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-06-14 12:10:05 +0200

    array added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 //JS RESET
 console.log('JS OK')
+
+//! OPERAZIONI INIZIALI:
+
+// CREO un array di 10 ggetti
+const zucchine = [
+    { varieta: 'Varietà 1', peso: 10, lunghezza: 4 },
+    { varieta: 'Varietà 2', peso: 13, lunghezza: 16 },
+    { varieta: 'Varietà 3', peso: 4, lunghezza: 23 },
+    { varieta: 'Varietà 4', peso: 11, lunghezza: 6 },
+    { varieta: 'Varietà 5', peso: 2, lunghezza: 17 },
+    { varieta: 'Varietà 6', peso: 5, lunghezza: 10 },
+    { varieta: 'Varietà 7', peso: 7, lunghezza: 9 },
+    { varieta: 'Varietà 8', peso: 3, lunghezza: 8 },
+    { varieta: 'Varietà 9', peso: 6, lunghezza: 27 },
+    { varieta: 'Varietà 10', peso: 14, lunghezza: 4 },
+  ];
